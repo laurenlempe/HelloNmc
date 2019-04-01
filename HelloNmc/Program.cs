0bbe29c51399e6166e51eb945b4dd1965e887cbd
Lauren Lempe <laurenlo02@gmail.com>
2019-04-01 14:06:00 -0400

    Add project files.

---
+++ HelloNmc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloNmc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*******************************
+            // App: Hello NMC
+            // Author: Lempe, Lauren
+            // Date: 4/1/19
+            //*******************************
+
+
+            Console.WriteLine();
+            Console.WriteLine("\t\t Hello NMC!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
